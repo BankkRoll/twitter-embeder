f70a8ca79f06d8d9b73071cafd7622ad1be02e72
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-02-06 09:34:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import discord
+from time import sleep
+import asyncio
+import tweepy
+
+
+# Setup Discord bot
+discord_token = os.environ['discord_token']
+client=discord.Client()
+
+#here is where you will put information for your twitter developer api keys
+#this will be twitter bot 1
+access_token_secret1 = os.environ['access_token_secret']
+access_token1 = os.environ['access_token']
+consumer_secret1 = os.environ['consumer_secret']
+consumer_key1 = os.environ['consumer_key']
+
+
+# Initiate Tweepy + Discord APIs
+auth=tweepy.OAuthHandler(consumer_key1,consumer_secret1)
+auth.set_access_token(access_token1,access_token_secret1)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+# Channel IDs
+discord_channel_id1='993955871438667926'
+discord_channel_id2='993977930386522173'
+discord_channel_id3='1026280943276798064'
+discord_channel_id4='1026283419551940639'
+
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    sleep(0.5)
+    print("A list of all servers where the bot is on:")
+    # List of all Servers the Bot is
+    insguilds = 0
+    for guild in client.guilds:
+      print(f"{guild.name} - {guild.id}")
+      insguilds = insguilds + 1
+      print("Guilds: " + str(insguilds))
+
+@client.event
+async def on_message(message):
+    # Ignore messages from self
+    if message.author == client.user:
+        return
+    # Extract needed variables
+    channel = str(message.channel.id)
+    # Channel ID 1
+    if channel == discord_channel_id1:
+        api.update_status(message.embeds[0].description)
+        print('Tweet posted from alien fren evo sales')
+    # Channel ID 2
+    if channel ==discord_channel_id2:
+        api.update_status(message.embeds[0].description)
+        print('Tweet posted from alien frens evo mints')
+    # Channel ID 3
+    if channel ==discord_channel_id3:
+        api.update_status(message.embeds[0].description)
+        print('Tweet posted from alien frens ufo sales')
+    # Channel ID 4
+    if channel ==discord_channel_id4:
+        api.update_status(message.embeds[0].description)
+        print('Tweet posted from frens orignals sale')
+
+    
+# Initiate bot
+async def main_func():
+    await client.start(discord_token)
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main_func())
